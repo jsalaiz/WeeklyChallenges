054f2117b1019e9596512e1853891ae01f8b96fe
Jonathan Salaiz <jonathan.salaiz@gmail.com>
2021-03-04 22:01:02 -0500

    Completed week challenge 3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -40,38 +40,62 @@ namespace ChallengesWithTestsMark8
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-
-            throw new NotImplementedException();
+            bool hasUpper = false;
+            bool hasLower = false;
+            bool hasDigit = false;
+            for (int i = 0; i < password.Length && !(hasUpper && hasLower && hasDigit); i++)
+            {
+                char c = password[i];
+                if (!hasUpper) hasUpper = char.IsUpper(c);
+                if (!hasLower) hasLower = char.IsLower(c);
+                if (!hasDigit) hasDigit = char.IsDigit(c);
+            }
+            if (hasUpper && hasLower && hasDigit) { return true; }
+            else { return false; }
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[val.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if(divisor == 0) { return 0; }
+            decimal result = dividend / divisor;
+            return result;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            int result = nums[nums.Length - 1] - nums[0];
+            return result;
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int[] answer = new int[50];
+            int j = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    answer[j] = i;
+                    j++;
+                }
+            }
+            return answer;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+                words[i] = words[i].ToUpper();
         }
     }
 }
